22a97aed6dd5683ff91bb4bacea2ad058ba19314
Hugo <>
2020-11-09 18:19:38 +0800

    :sparkles: Wrap component with forwardRef

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 // rel="external" when you trust the link
-function ExternalLink ({ children, ...attr }) {
-  return (
-    <a target="_blank" rel="noopener noreferrer" itemProp="url" {...attr}>
+const ExternalLink = React.forwardRef(
+  function ExternalLink ({ children, ...attr }, ref) {
+    return (
+    <a ref={ref} target="_blank" rel="noopener noreferrer" itemProp="url" {...attr}>
       {children}
     </a>
-  )
-}
+    )
+  })
 
 export default ExternalLink
